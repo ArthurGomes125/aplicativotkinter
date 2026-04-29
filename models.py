6eec7fa209cb8814f80040e9308530508e4d8eba
Bryan Natallycio <natallyciob12123434@gmail.com>
2026-04-29 09:38:30 -0300

    Create models.py for data models for User and Product

---
+++ models.py
@@ -0,0 +1,11 @@
+# Data Models for User and Product
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
